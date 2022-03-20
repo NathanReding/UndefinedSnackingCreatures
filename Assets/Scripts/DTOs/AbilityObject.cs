b6bf28d190ad01fb0f2dfee31b044a5624892904
Nathan Reding <reding.nathan@gmail.com>
2022-03-19 23:35:41 -0500

    minor changes

--- Assets/Scripts/DTOs/AbilityObject.cs
+++ Assets/Scripts/DTOs/AbilityObject.cs
@@ -7,22 +7,20 @@ namespace DTOs
     [System.Serializable]
     class AbilityObject
     {
-        AbilityTemplate template;
-        ConditionalArgSet incomingArgSet;
-        List<ITargetableObject> targets;
-        ITargetableObject origin;
-        List<EffectObject> effects;
+        public AbilityTemplate template;
+        public ConditionalArgSet incomingArgSet;
+        public List<ITargetableObject> targets;
+        public ITargetableObject origin;
+        public List<EffectObject> effects;
 
         public AbilityObject(AbilityTemplate template, System.Object incomingArgSet){
             if(incomingArgSet.GetType() != typeof(ConditionalArgSet)){
                 Debug.LogError("AbilityObject constructed with illegal type for incomingArgSet. type: " + incomingArgSet.GetType());
                 return;
             }
-            effects = new List<(string, string)>();
+            effects = new List<EffectObject>();
             targets = new List<ITargetableObject>();
         }
-
-
 
     }
 }
--- Assets/Scripts/ReactionObserver.cs
+++ Assets/Scripts/ReactionObserver.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using DTOs;
+using SharedScripts;
+using UnityEngine;
 
 public class ReactionObserver
 {
@@ -17,24 +20,59 @@ public class ReactionObserver
     Dictionary<int, List<ReactionObject>> timeTriggeredRemoval;
     int currentTurn = 1;
 
-    Dictionary<CreatureObject, List<ReactionObject>> modificationReactions;
-    Dictionary<CreatureObject, List<ReactionObject>> replacementReactions;
-    Dictionary<CreatureObject, List<ReactionObject>> absoluteReactions;
+    Dictionary<ITargetableObject, List<ReactionObject>> modificationReactions;
+    Dictionary<ITargetableObject, List<ReactionObject>> replacementReactions;
+    Dictionary<ITargetableObject, List<ReactionObject>> absoluteReactions;
 
     private ReactionObserver()
     {
-        removalEvents = new Dictionary<string, List<ReactionObject>>();
+        removalTriggeredEvents = new Dictionary<string, List<ReactionObject>>();
+        timeTriggeredRemoval = new Dictionary<int, List<ReactionObject>>();
 
-        modificationReactions = new Dictionary<CreatureObject, List<ReactionObject>>();
-        replacementReactions = new Dictionary<CreatureObject, List<ReactionObject>>();
-        absoluteReactions = new Dictionary<CreatureObject, List<ReactionObject>>();
+        modificationReactions = new Dictionary<ITargetableObject, List<ReactionObject>>();
+        replacementReactions = new Dictionary<ITargetableObject, List<ReactionObject>>();
+        absoluteReactions = new Dictionary<ITargetableObject, List<ReactionObject>>();
     }
 
-    public List<(string, string)> getModReactions(AbilityObject ability){
+    public List<EffectObject> getModReactions(System.Object obj)
+    {
+        if (SharedCode.verifyTyping(obj, typeof(AbilityObject), "")) return null;
+        AbilityObject ability = obj as AbilityObject;
+        List<EffectObject> result = new List<EffectObject>();
+        List<string> triggers = new List<string>();
+        foreach (EffectObject effect in ability.effects)
+        {
+            triggers.Add(effect.name);
+        }
         // check reciever list for this creature
-        modificationReactions
-        // check 
+        foreach (ITargetableObject targitable in ability.targets)
+        {
+            if (modificationReactions.ContainsKey(targitable))
+            {
+                result.AddRange(getReliventReactions(modificationReactions[targitable], true, triggers));
+            }
+        }
+        if (modificationReactions.ContainsKey(ability.origin))
+        {
+            result.AddRange(getReliventReactions(modificationReactions[ability.origin], false, triggers));
+        }
+        return result;
     }
+
+
+    private List<EffectObject> getReliventReactions(List<ReactionObject> reactions, bool typeReciever, List<string> triggers)
+    {
+        List<EffectObject> result = new List<EffectObject>();
+        foreach (ReactionObject reaction in reactions)
+        {
+            if (reaction.typeReciever == typeReciever && triggers.Contains(reaction.trigger))
+            {
+                result.AddRange(reaction.effects);
+            }
+        }
+        return result;
+    }
+
     /*
         The reactions has a list that will be removed at x turns
         The reactions need to know what triggers it
@@ -46,53 +84,64 @@ public class ReactionObserver
 
     */
 
-    public void addReaction(string trigger, string ractionType, CreatureObject owner, bool typeReciever, string persistFor, int durationValue, List<EffectObject> effects){
+    public void addReaction(string trigger, string reactionType, ITargetableObject owner, bool typeReciever, string persistFor, int durationValue, List<EffectObject> effects)
+    {
         ReactionObject newReaction = new ReactionObject(
             trigger, reactionType, owner, typeReciever, persistFor, durationValue, effects
-        )
-        
-        Dictionary<CreatureObject, List<ReactionObject>> dictionary = getDictionary(ractionType);
-        if(dictionary.ContainsKey(newReaction.owner)){
+        );
+
+        Dictionary<ITargetableObject, List<ReactionObject>> dictionary = getDictionary(reactionType);
+        if (dictionary.ContainsKey(newReaction.owner))
+        {
             dictionary[newReaction.owner].Add(newReaction);
         }
-        else{
-            dictionary.Add(owner, new List<ReactionObject>(){newReaction});
+        else
+        {
+            dictionary.Add(owner, new List<ReactionObject>() { newReaction });
         }
 
-        if(newReaction.persistFor == "forTurns"){
+        if (newReaction.persistFor == "forTurns")
+        {
             int turnValue = currentTurn + durationValue;
-            if(durationValue <= 0){
-                Debug.ErrorLog("durationValue not valid. Continued equil to 1");
+            if (durationValue <= 0)
+            {
+                Debug.LogError("durationValue not valid. Continued equil to 1");
                 turnValue = currentTurn + 1;
             }
-            if(timeTriggeredRemoval.ContainsKey(turnValue
-            )){
+            if (timeTriggeredRemoval.ContainsKey(turnValue
+            ))
+            {
                 timeTriggeredRemoval[turnValue].Add(newReaction);
             }
-            else{
-                timeTriggeredRemoval.Add(turnValue, new List<ReactionObject>(){newReaction});
+            else
+            {
+                timeTriggeredRemoval.Add(turnValue, new List<ReactionObject>() { newReaction });
             }
         }
-        else if(removalEvents.ContainsKey(newReaction.persistFor
-        )){
-            removalEvents[newReaction.persistFor].Add(newReaction);
+        else if (removalTriggeredEvents.ContainsKey(newReaction.persistFor
+        ))
+        {
+            removalTriggeredEvents[newReaction.persistFor].Add(newReaction);
         }
-        else{
-            removalEvents.Add(persistFor, new List<ReactionObject>(){newReaction});
+        else
+        {
+            removalTriggeredEvents.Add(persistFor, new List<ReactionObject>() { newReaction });
         }
 
     }
 
-    private class ReactionObject{
+    private class ReactionObject
+    {
         public string trigger;
         public string reactionType;
-        public  CreatureObject owner;
-        public  bool typeReciever;
+        public ITargetableObject owner;
+        public bool typeReciever;
         public string persistFor;
         public int durationValue;
         public List<EffectObject> effects;
 
-        public ReactionObject(string trigger, string reactionType, CreatureObject owner, bool typeReciever, string persistFor, int durationValue, List<EffectObject> effects){
+        public ReactionObject(string trigger, string reactionType, ITargetableObject owner, bool typeReciever, string persistFor, int durationValue, List<EffectObject> effects)
+        {
             this.trigger = trigger;
             this.reactionType = reactionType;
             this.owner = owner;
@@ -103,9 +152,11 @@ public class ReactionObserver
         }
     }
 
-
-    private Dictionary<CreatureObject, List<ReactionObject>> getDictionary(string ractionType){
-            switch(ractionType){
+    // TODO
+    private Dictionary<ITargetableObject, List<ReactionObject>> getDictionary(string ractionType)
+    {
+        switch (ractionType)
+        {
             case "modification":
                 return modificationReactions;
             case "replacement":
@@ -117,37 +168,46 @@ public class ReactionObserver
 
 
 
-    public void moveToNextTurn(){
+    public void moveToNextTurn()
+    {
         currentTurn++;
-        if(timeTriggeredRemoval.ContainsKey(currentTurn)){
-            foreach(ReactionObject obj in timeTriggeredRemoval[currentTurn]){
+        if (timeTriggeredRemoval.ContainsKey(currentTurn))
+        {
+            foreach (ReactionObject obj in timeTriggeredRemoval[currentTurn])
+            {
                 removeEvent(obj);
             }
             timeTriggeredRemoval.Remove(currentTurn);
         }
     }
 
-    public void removeEndOfturnReactions(){
-        foreach(ReactionObject obj in removalTriggeredEvents["tillEndOfTurn"]){
+    public void removeEndOfturnReactions()
+    {
+        foreach (ReactionObject obj in removalTriggeredEvents["tillEndOfTurn"])
+        {
             removeEvent(obj);
         }
         removalTriggeredEvents["tillEndOfTurn"] // replace this field with a new list
     }
 
-    public void removeEndOfMatchReactions(){
-        foreach(ReactionObject obj in removalTriggeredEvents["tillEndOfMatch"]){
+    public void removeEndOfMatchReactions()
+    {
+        foreach (ReactionObject obj in removalTriggeredEvents["tillEndOfMatch"])
+        {
             removeEvent(obj);
         }
         removalTriggeredEvents["tillEndOfMatch"] // replace this field with a new list
     }
 
 
-    private void removeEvent(ReactionObject obj){
+    private void removeEvent(ReactionObject obj)
+    {
         removeEventFromDictionary(obj, getDictionary(obj.reactionType));
     }
 
-    private void removeEventFromDictionary(ReactionObject obj, Dictionary<CreatureObject, List<ReactionObject>> dictionary){
+    private void removeEventFromDictionary(ReactionObject obj, Dictionary<ITargetableObject, List<ReactionObject>> dictionary)
+    {
         dictionary[obj.owner].Remove(obj);
-        if(dictionary[obj.owner].Count <= 0) dictionary.Remove(obj.owner);
+        if (dictionary[obj.owner].Count <= 0) dictionary.Remove(obj.owner);
     }
 }
